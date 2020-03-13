04a312b463a3e4d0f7bb94fa8e1992dcf32ef766
fares siouane <fares.geek.dz@gmail.com>
2020-03-13 16:04:28 +0100

    setup api with basic formatting

--- quotes/views.py
+++ quotes/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    import requests
+    import json
+    
+    #pk_4335e55ab8444511a9ad95399164cb69
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_4335e55ab8444511a9ad95399164cb69")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error"
+    
+    return render(request, 'home.html', {'api' : api,})
