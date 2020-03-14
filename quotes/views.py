492ea685909353ad7acad59ef3b2185c529b4a74
fares siouane <fares.geek.dz@gmail.com>
2020-03-14 15:58:36 +0100

    setup stock_add form

--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Stock
+from .forms import StockForm
 # Create your views here.
 
 def home(request):
@@ -25,18 +27,30 @@ def home(request):
 
 
 def stock(request):
-    import requests
-    import json
+    if request.method == "POST":
+        form = StockForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'successfully, added new ticker!')
+        else:
+            messages.success(request, 'could not add your ticker, please try gain!')
 
-    symbols = Stock.objects.all()
-    tickers = []
-    for symbol in symbols:
-        api_request = requests.get(f"https://cloud.iexapis.com/stable/stock/{symbol}/quote?token=pk_4335e55ab8444511a9ad95399164cb69")
+        return redirect('stock')
 
-        try:
-            api = json.loads(api_request.content)
-            tickers.append(api)
-        except Exception as e:
-            api = "Error"
+    else:
+        import requests
+        import json
 
-    return render(request, 'stock.html', {'tickers' : tickers})
+        symbols = Stock.objects.all()
+        tickers = []
+        for symbol in symbols:
+            api_request = requests.get(f"https://cloud.iexapis.com/stable/stock/{symbol}/quote?token=pk_4335e55ab8444511a9ad95399164cb69")      
+            #api = json.loads(api_request.content)
+            try:
+                api = json.loads(api_request.content)
+                tickers.append(api)
+            except Exception as e:
+                api = "Error"
+        
+
+        return render(request, 'stock.html', {'tickers' : tickers})
