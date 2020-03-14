55b669b9a6a75c0c08e68f92d8edbe46fd783c4b
fares siouane <fares.geek.dz@gmail.com>
2020-03-14 13:30:11 +0100

    set up search funtionality via symbol

--- quotes/views.py
+++ quotes/views.py
@@ -3,15 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    import requests
-    import json
-    
-    #pk_4335e55ab8444511a9ad95399164cb69
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_4335e55ab8444511a9ad95399164cb69")
+    if request.method == "POST":
+        import requests
+        import json
+        
+        #pk_4335e55ab8444511a9ad95399164cb69
+        symbol = request.POST['query']
+        api_request = requests.get(f"https://cloud.iexapis.com/stable/stock/{symbol}/quote?token=pk_4335e55ab8444511a9ad95399164cb69")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error"
-    
-    return render(request, 'home.html', {'api' : api,})
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error"
+        
+        return render(request, 'home.html', {'api' : api,})
+
+
+    else:
+        return render(request, 'home.html')
